e781b822a0b28aa062436d3f9b220b87817c11c5
Marko Tsymbaluk <markotsymbaluk@gmail.com>
2025-12-04 01:26:03 -0500

    Refactor MainMenu layout: responsive grid system and UI improvements
    - Convert to CSS Grid layout with responsive button sizing
    - Remove arbitrary positioning and scrolling issues
    - Center buttons horizontally and vertically
    - Make buttons and icons responsive to screen size
    - Add Wikipedia SVG favicon
    - Remove play button navigation functionality
    - Adjust button gaps, icon sizes, and text styling
    - Align header text with grid cells
    - Add Wikipedia logo assets

--- src/pages/MainMenu.tsx
+++ src/pages/MainMenu.tsx
@@ -6,15 +6,8 @@ import { IoIosSettings } from "react-icons/io";
 import { FaGithub } from "react-icons/fa";
 import { SiWikipedia } from "react-icons/si";
 import ArticlePreview from "../components/ArticlePreview";
-import { useNavigate } from "react-router-dom";
 
 const MainMenu = () => {
-  const navigate = useNavigate();
-
-  const handlePlayClick = () => {
-    navigate("/game");
-  };
-
   return (
     <div className="container">
       <div className="menu-header">
@@ -24,43 +17,45 @@ const MainMenu = () => {
           your destination.
         </p>
       </div>
-      <div
-        className="mail"
-        onClick={handlePlayClick}
-        style={{ cursor: "pointer" }}
-      >
-        <i className="fa fa-play fa-3x mailIcon"></i>
-        <span className="mailmail">Single Player</span>
-      </div>
+      <div className="button-grid">
+        <div className="mail">
+          <i className="fa fa-play fa-3x mailIcon"></i>
+          <span className="mailmail">Single Player</span>
+        </div>
 
-      <div className="skype">
-        <img src={fightIcon} alt="Duel" className="skypeIcon" />
-        <span className="skypeskype">Duel</span>
-      </div>
+        <div className="calendar">
+          <img
+            src={multiplayerIcon}
+            alt="Multiplayer"
+            className="calendarIcon"
+          />
+          <span className="calendarDay">Multiplayer</span>
+        </div>
 
-      <div className="map">
-        <IoCalendarNumberSharp className="mapIcon" />
-        <span className="mapmap">Daily</span>
-      </div>
+        <div className="photos">
+          <FaGithub className="photoIcon" />
+          <span className="photophoto">GitHub</span>
+        </div>
 
-      <div className="calendar">
-        <img src={multiplayerIcon} alt="Multiplayer" className="calendarIcon" />
-        <span className="calendarDay">Multiplayer</span>
-      </div>
+        <div className="skype">
+          <img src={fightIcon} alt="Duel" className="skypeIcon" />
+          <span className="skypeskype">Duel</span>
+        </div>
 
-      <div className="sports">
-        <IoIosSettings className="sportsIcon" />
-        <span className="sportsLabel">Settings</span>
-      </div>
+        <div className="map">
+          <IoCalendarNumberSharp className="mapIcon" />
+          <span className="mapmap">Daily</span>
+        </div>
 
-      <div className="photos">
-        <FaGithub className="photoIcon" />
-        <span className="photophoto">GitHub</span>
-      </div>
+        <div className="sports">
+          <IoIosSettings className="sportsIcon" />
+          <span className="sportsLabel">Settings</span>
+        </div>
 
-      <div className="help">
-        <SiWikipedia className="helpIcon" />
-        <span className="helphelp">Today's Article</span>
+        <div className="help">
+          <SiWikipedia className="helpIcon" />
+          <span className="helphelp">Today's Article</span>
+        </div>
       </div>
 
       <div className="menu-article-preview">
